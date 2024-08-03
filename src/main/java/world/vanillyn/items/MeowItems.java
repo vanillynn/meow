3ba5811dcd1c8c174f7490deb4a969f5fe39cd12
v <vanillyn@multiryzz.ch>
2024-08-02 23:53:48 -0400

    update itemgroups

--- src/main/java/world/vanillyn/items/MeowItems.java
+++ src/main/java/world/vanillyn/items/MeowItems.java
@@ -1,49 +1,41 @@
 package world.vanillyn.items;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-
 
 import static world.vanillyn.Meow.MOD_ID;
 import static world.vanillyn.Meow.LOGGER;
 
 public class MeowItems {
-    public static final RegistryKey<ItemGroup> MEOW_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(MOD_ID, "item_group"));
-    public static final ItemGroup MEOW_GROUP = FabricItemGroup.builder()
-            .icon(() -> new ItemStack(MeowItems.MEOW_SWORD))
-            .displayName(Text.translatable("item_group.meow"))
-            .build();
-    public static <T extends Item> T register(T item, String id){
+
+    public static <T extends Item> T register(T item, String id, RegistryKey<ItemGroup> group){
         // Create the identifier for the item.
         Identifier itemID = Identifier.of(MOD_ID, id);
 
-        ItemGroupEvents.modifyEntriesEvent(MEOW_GROUP_KEY).register(itemGroup -> itemGroup.add(item));
+        ItemGroupEvents.modifyEntriesEvent(group).register(itemGroup -> itemGroup.add(item));
 
         return Registry.register(Registries.ITEM, itemID, item);
     }
     public static void initialize() {
         LOGGER.info("Items initialized!");
-
     }
 
     // meow needle
-    public static final Item NEEDLE = register(new Needle(new Item.Settings().maxCount(32)  ), "needle");
-    public static final Item MEOW_SUBSTANCE = register(new MeowSubstance(new Item.Settings()), "meow_substance");
-    public static final Item DRELIUM = register(new Drelium(new Item.Settings()), "drelium");
-    public static final Item EMPTY_NEEDLE = register(new EmptyNeedle(new Item.Settings()), "empty_needle");
+    public static final Item NEEDLE = register(new Needle(new Item.Settings().maxCount(32)  ), "needle", ItemGroups.TOOLS);
+    public static final Item MEOW_SUBSTANCE = register(new MeowSubstance(new Item.Settings()), "meow_substance", ItemGroups.INGREDIENTS);
+    public static final Item DRELIUM = register(new Drelium(new Item.Settings()), "drelium", ItemGroups.INGREDIENTS);
+    public static final Item EMPTY_NEEDLE = register(new EmptyNeedle(new Item.Settings()), "empty_needle", ItemGroups.INGREDIENTS);
 
     // Tools and Material
-    public static final Item MEOW_INGOT = register(new MeowIngot(new Item.Settings()), "meow_ingot");
-    public static final Item MEOW_SWORD = register(new SwordItem(MeowMaterial.INSTANCE, new Item.Settings()), "meow_sword");
-    public static final Item MEOW_PICKAXE = register(new PickaxeItem(MeowMaterial.INSTANCE, new Item.Settings()), "meow_pickaxe");
-    public static final Item MEOW_SHOVEL = register(new ShovelItem(MeowMaterial.INSTANCE, new Item.Settings()), "meow_shovel");
-    public static final Item MEOW_AXE = register(new AxeItem(MeowMaterial.INSTANCE, new Item.Settings()), "meow_axe");
+    public static final Item MEOW_INGOT = register(new MeowIngot(new Item.Settings()), "meow_ingot", ItemGroups.INGREDIENTS);
+    public static final Item MEOW_SWORD = register(new SwordItem(MeowMaterial.INSTANCE, new Item.Settings()), "meow_sword", ItemGroups.COMBAT);
+    public static final Item MEOW_PICKAXE = register(new PickaxeItem(MeowMaterial.INSTANCE, new Item.Settings()), "meow_pickaxe", ItemGroups.TOOLS);
+    public static final Item MEOW_SHOVEL = register(new ShovelItem(MeowMaterial.INSTANCE, new Item.Settings()), "meow_shovel", ItemGroups.TOOLS);
+    public static final Item MEOW_AXE = register(new AxeItem(MeowMaterial.INSTANCE, new Item.Settings()), "meow_axe", ItemGroups.TOOLS);
 
 
 
